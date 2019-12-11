b6018556945d4e88d527144ca29c6f70014fb306
justcreateMZ <58639317+justcreateMZ@users.noreply.github.com>
2019-12-11 15:38:12 +0100

    Shelve implementation
    changed txt file to shelve for storing score and history

--- asr_counter.py
+++ asr_counter.py
@@ -1,40 +1,32 @@
 #! python3
 # asr_counter counts number of done asr-s in ditto.av (my working environment)
 
-import os
 import datetime
+import shelve
+import os
 
 
 def score():  # shows current asr score
-    asr = open('asr.txt', 'r')
-    asr_score = asr.read()
+    asr = shelve.open('asr')
+    print('\nDivision: ' + str(asr['d']))
+    print('Codes: ' + str(asr['c']))
+    print('Points: ' + str(asr['d'] + asr['c'] / 2) + '\n')
     asr.close()
-    division = list(asr_score).count('d')
-    codes = list(asr_score).count('c')
-    print('\nDivision: ' + str(division))
-    print('Codes: ' + str(codes))
-    print('Points: ' + str(division + codes / 2) + '\n')
 
 
 def delete_last():  # deletes last entered thing
-    asr = open('asr.txt', 'r')
-    asr_score = asr.read()
+    asr = shelve.open('asr')
+    asr_history = asr['history']
+    if not asr_history == '':
+        asr[asr_history[-1]] -= 1
+        asr['history'] = asr_history[:-1]
+        print('Last step deleted')
     asr.close()
-    asr_score = list(asr_score)
-    try:
-        asr_score.pop()
-    except IndexError:
-        print('Score already empty')
-        return
-    asr = open('asr.txt', 'w')
-    asr.write(''.join(asr_score))
-    asr.close()
-    print('Last step deleted')
 
 
 def counting():     # counting asr-s
-    print('"d" for division\n"c" for codes\n"reverse" to reverse last step\n"end" to end\n')
     score()
+    print('"d" for division\n"c" for codes\n"reverse" to reverse last step\n"end" to end\n')
     while True:
         asr_type = input().lower()
         if asr_type == 'end':
@@ -42,42 +34,50 @@ def counting():     # counting asr-s
         if asr_type == 'reverse':
             delete_last()
         if asr_type in ['d', 'c']:
-            asr = open('asr.txt', 'a')
-            asr.write(asr_type)
+            asr = shelve.open('asr')
+            asr[asr_type] += 1
+            asr['history'] += asr_type
             asr.close()
             print(datetime.datetime.now().strftime('%H:%M:%S'))
         score()
     score()
 
 
+def reset():    # resets score (and used when run for the first time)
+    asr = shelve.open('asr')
+    asr['d'] = 0
+    asr['c'] = 0
+    asr['history'] = ''
+    asr.close()
+
+
+# main program
 print('Hello! What do you want me to do?')
 print('Type "count" to enable count mode')
 print('Type "reset" to reset asr counter')
 
-if not os.path.exists(os.getcwd() + '\\asr.txt'):   # checks whether .txt exists
-    asr = open('asr.txt', 'w')
-    asr.close()
+# creates shelf when first run
+if not (os.path.exists(os.getcwd() + '\\asr.db') or os.path.exists(os.getcwd() + '\\asr.bak')):
+    reset()
 
 while True:     # gets what user wants to do
-    action = input()
-    if action.lower() in ['count', 'reset']:
+    action = input().lower()
+    if action in ['count', 'reset']:
         break
 
-if action.lower() == 'count':
+if action == 'count':
     counting()
 
-if action.lower() == 'reset':   # resets score
-    asr = open('asr.txt', 'w')
-    asr.write('')
-    asr.close()
+if action == 'reset':   # resets score
+    reset()
     print('Reset done')
 
 input('\nPress enter to exit')
-
-# TODO change normal open to shelve (import shelve)
 
 # TODO: (import keyboard)
 '''https://stackoverflow.com/questions/48915822/
 creating-a-hotkey-to-enter-text-using-python-running-in-background-waiting-for'''
 
 # TODO compile it
+
+# TODO ditto sniffer
